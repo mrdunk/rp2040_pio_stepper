3ab3a00d5d53625ceb27507578fbdd6ba7ac1490
duncan law <mrdunk@gmail.com>
2023-02-20 22:19:21 +0000

    Parse UART and network.

--- pico_stepper/pico_stepper.c
+++ pico_stepper/pico_stepper.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "pico/stdlib.h"
 
 // PIO related.
@@ -11,7 +14,8 @@
 #include "port_common.h"
 #include "wizchip_conf.h"
 #include "w5x00_spi.h"
-#include "loopback.h"
+//#include "loopback.h"
+#include "socket.h"
 
 /* Clock */
 #define PLL_SYS_KHZ (133 * 1000)
@@ -20,12 +24,13 @@
 #define ETHERNET_BUF_MAX_SIZE (1024 * 2)
 
 /* Socket */
-#define SOCKET_LOOPBACK 0
+#define SOCKET_NUMBER 0
 
 /* Port */
-#define PORT_LOOPBACK 5000
+#define NW_PORT 5000
 
-#define NET_ENABLE 0
+#define NET_ENABLE 1
+#define DATA_BUF_SIZE 255
 
 const uint LED_PIN = 25;
 
@@ -66,17 +71,146 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
 
     printf("Blinking pin %d at %d Hz\n", pin, freq);
 
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    //pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
-    uint32_t level = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
-    pio_sm_put_blocking(pio, sm, level);
-    pio_sm_put_blocking(pio, sm, 3);
+    uint32_t stepper_num = 0;
+    uint32_t direction = 0;
+    uint32_t pulse_len = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
+    uint32_t pulse_count = 10;
+    pio_sm_put_blocking(pio, sm, stepper_num);
+    pio_sm_put_blocking(pio, sm, direction);
+    pio_sm_put_blocking(pio, sm, pulse_len);
+    pio_sm_put_blocking(pio, sm, pulse_count);
 }
+
+void send_data(uint32_t *values) {
+  printf("%ld:%ld:%ld:%ld\n", values[0], values[1], values[2], values[3]);
+}
+
+void process_buffer(char* uart_buffer) {
+  printf("\nReceived: %s\n", uart_buffer);
+  uint32_t values[4] = {0,0,0};
+  size_t value_num = 0;
+  char* itterate = uart_buffer;
+  uint32_t val = 0;
+  while(*itterate) {
+    if (*itterate == ':') {
+      // Separator.
+      printf(":\n");
+      itterate++;
+    } else if (isspace(*itterate)) {
+      // Whitespace. Ignore and continue.
+      itterate++;
+    } else if (isdigit(*itterate)) {
+      val = strtol(itterate, &itterate, 10);
+      if (value_num < 4) {
+        printf("%ld\n", val);
+        values[value_num] = val;
+      } else {
+        printf("Too many values. %ld\n", val);
+        return;
+      }
+      value_num++;
+    } else {
+      printf("Unexpected character: %u : %c\n", *itterate, *itterate);
+      return;
+    }
+  }
+
+  if (value_num == 4) {
+    send_data(values);
+  } else {
+    printf("Wrong number of values: %i\n", value_num);
+  }
+}
+
+uint8_t get_uart(char* uart_buffer, size_t buffer_len) {
+    char* start_update = &uart_buffer[strlen(uart_buffer)];
+
+    int ch = getchar_timeout_us(0);
+    while (ch > 0) {
+      size_t pos = strlen(uart_buffer);
+      if (ch == 13) {
+        // Return pressed.
+        return 1;
+      } else if (pos < buffer_len - 1) {
+        uart_buffer[pos] = ch;
+        printf("%c", ch);
+      }
+      ch = getchar_timeout_us(0);
+    }
+
+    return 0;
+}
+
+void collect_uart_buffer(char* uart_buffer, size_t buffer_len) {
+    uint8_t buffer_state = get_uart(uart_buffer, DATA_BUF_SIZE);
+    if (buffer_state > 0) {
+      process_buffer(uart_buffer);
+      memset(uart_buffer, '\0', DATA_BUF_SIZE);
+    }
+}
+
+int32_t collect_nw_buffer(uint8_t socket_num, uint8_t* network_buffer, uint16_t port) {
+  int32_t ret;
+  uint16_t size_received = 0;
+
+  uint8_t destip[4];
+  uint16_t destport;
+
+  switch(getSn_SR(socket_num))
+  {
+    case SOCK_ESTABLISHED :
+      if(getSn_IR(socket_num) & Sn_IR_CON) {
+        getSn_DIPR(socket_num, destip);
+        destport = getSn_DPORT(socket_num);
+
+        printf("%d:Connected - %d.%d.%d.%d : %d\r\n",
+            socket_num, destip[0], destip[1], destip[2], destip[3], destport);
+        setSn_IR(socket_num,Sn_IR_CON);
+      }
+      if((size_received = getSn_RX_RSR(socket_num)) > 0) {
+        if(size_received > DATA_BUF_SIZE) {
+          size_received = DATA_BUF_SIZE;
+        }
+        ret = recv(socket_num, network_buffer, size_received);
+
+        if(ret <= 0) {
+          return ret;
+        }
+
+        if (strlen(network_buffer) > 0) {
+          process_buffer(network_buffer);
+        }
+      }
+      break;
+    case SOCK_CLOSE_WAIT :
+      if((ret = disconnect(socket_num)) != SOCK_OK) {
+        return ret;
+      }
+      printf("%d:Socket Closed\r\n", socket_num);
+      break;
+    case SOCK_INIT :
+      printf("%d:Listen, TCP server loopback, port [%d]\r\n", socket_num, port);
+      if( (ret = listen(socket_num)) != SOCK_OK) {
+        return ret;
+      }
+      break;
+    case SOCK_CLOSED:
+      if((ret = socket(socket_num, Sn_MR_TCP, port, 0x00)) != socket_num) {
+        return ret;
+      }
+      break;
+    default:
+      break;
+  }
+  return 1;
+}
+
 
 int main() {
 
   int retval = 0;
+  char uart_buffer[DATA_BUF_SIZE] = "";
+  char network_buffer[DATA_BUF_SIZE] = "";
 
   bi_decl(bi_program_description("This is a test binary."));
   bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
@@ -96,9 +230,10 @@ int main() {
     print_network_information(g_net_info);
   }
 
+  stdio_usb_init();
   setup_default_uart();
 
-  // PIO init. 
+  // PIO init.
   PIO pio = pio0;
   uint offset = pio_add_program(pio, &blink_program);
   printf("Loaded program at %d\n", offset);
@@ -109,14 +244,18 @@ int main() {
   blink_pin_forever(pio, 3, offset, 3, 4);
 
 
+  sleep_ms(1000);
+  puts("Hello World");
+
   while (1) {
-    puts("Hello World\n");
-    sleep_ms(1000);
 
     if (NET_ENABLE &&
-        (retval = loopback_tcps(SOCKET_LOOPBACK, g_loopback_buf, PORT_LOOPBACK)) < 0) {
+        (retval = collect_nw_buffer(SOCKET_NUMBER, network_buffer, NW_PORT)) < 0) {
       printf(" Loopback error : %d\n", retval);
       while (1);
     }
+
+    collect_uart_buffer(uart_buffer, DATA_BUF_SIZE);
+
   }
 }
